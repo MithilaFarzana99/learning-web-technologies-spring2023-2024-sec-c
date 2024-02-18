58fe3b2ca817469c7806893f4545150ef0bbe016
MithilaFarzana99 <mithila260@icloud.com>
2024-02-18 16:47:23 +0600

    pushing labtask 5

---
+++ Lab5/task1.php
@@ -0,0 +1,18 @@
+<?php
+ echo "<html>
+ <head>
+<title>LOGIN</title>
+</head>
+ <form method = "post" action= "task1.php">
+     <fieldset>
+         <legend> LOGIN </legend>
+     User Name: <input type="text" name= "username" value= ""/> <br>
+     Password: <input type="password" name= "password" value= ""/> <br>
+      <input type="checkbox" name= "remember" value=""/> Remember Me <br>
+      <input type="submit" name= "button" value="Submit"/> 
+      <a href = task2.html> Forgot Password </a> <br>
+      </fieldset>
+ </form>
+
+</html>
+"; 
